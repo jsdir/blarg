31c3b05d6f5df3fb8a4473058af1bece45cebae3
Jason Sommer <j@jsdir.net>
2016-10-26 23:04:23 -0500

    Update

--- middleware.go
+++ middleware.go
@@ -5,12 +5,24 @@ import (
 	"strings"
 )
 
+func isHTTPS(r *http.Request) bool {
+	if r.URL.Scheme == "https" {
+		return true
+	}
+	if strings.HasPrefix(r.Proto, "HTTPS") {
+		return true
+	}
+	if r.Header.Get("X-Forwarded-Proto") == "https" {
+		return true
+	}
+	return false
+}
+
 func ForceHTTPS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		url := r.URL
 		// thx: https://github.com/unrolled/secure
-		proto := r.Header.Get("X-Forwarded-Proto")
-		if !strings.EqualFold(proto, "https") {
+		if !isHTTPS(r) {
 			url.Scheme = "https"
 			url.Host = r.Host
 			http.Redirect(w, r, url.String(), http.StatusMovedPermanently)
