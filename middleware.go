476826586f398b656fa19bdcaf3874d3db07ea39
Jason Sommer <j@jsdir.net>
2016-10-26 23:01:06 -0500

    Use X-Forwarded-Proto

--- middleware.go
+++ middleware.go
@@ -9,7 +9,8 @@ func ForceHTTPS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		url := r.URL
 		// thx: https://github.com/unrolled/secure
-		if !strings.EqualFold(url.Scheme, "https") {
+		proto := r.Header.Get("X-Forwarded-Proto")
+		if !strings.EqualFold(proto, "https") {
 			url.Scheme = "https"
 			url.Host = r.Host
 			http.Redirect(w, r, url.String(), http.StatusMovedPermanently)
