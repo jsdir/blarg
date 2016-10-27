2ceefd4f18b419966eb014ecd8c67495f0d63440
Jason Sommer <j@jsdir.net>
2016-10-26 22:26:17 -0500

    Fix redirect logicc

--- middleware.go
+++ middleware.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"net/http"
+	"strings"
 )
 
 func ForceHTTPS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("X-Forwarded-Proto") != "https" {
-			url := r.URL
-			if url.Scheme != "" {
-				url.Scheme = "https"
-			}
-			http.Redirect(w, r, url.String(), 301)
-		} else {
-			h.ServeHTTP(w, r)
+		url := r.URL
+		// thx: https://github.com/unrolled/secure
+		if !strings.EqualFold(url.Scheme, "https") {
+			url.Scheme = "https"
+			url.Host = r.Host
+			http.Redirect(w, r, url.String(), http.StatusMovedPermanently)
+			return
 		}
+
+		h.ServeHTTP(w, r)
 	})
 }
